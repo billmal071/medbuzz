60f743baa27dddc2fc7ffe90cb8951951722a625
billmal <billmal071@gmail.com>
2020-07-26 18:57:59 +0100

    added JS email validator

--- assets/js/medbuzz.js
+++ assets/js/medbuzz.js
@@ -12,4 +12,19 @@ function scrollFunction() {
 }
 
 //email validation
-//emailForm = document.forms["contact"]["email"].value;
+document.getElementById("submitBtn").addEventListener("click", emailValidation);
+let mailFormat = "[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$";
+
+function emailValidation() {
+  //console.log(document.getElementById("emailTxt").value);
+  let emailForm = document.getElementById("emailTxt").value;
+  if (emailForm == "" || !emailForm.match(mailFormat)) {
+    //alert("Please enter a valid email");
+    document.getElementById("errorMsg").classList.add("show-error");
+    document.getElementById("errorMsg").classList.remove("d-none");
+  }
+  document.getElementById("closeErr").addEventListener("click", () => {
+    document.getElementById("errorMsg").classList.remove("show-error");
+    document.getElementById("errorMsg").classList.add("d-none");
+  })
+}
